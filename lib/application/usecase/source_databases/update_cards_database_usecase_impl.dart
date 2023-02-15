4152212e3b245b04bd424750207f354b914282d6
Derek Alves <dev.derek.alves@gmail.com>
2023-02-15 15:58:39 -0300

    refactor: remove url maneger from param

--- lib/application/usecase/source_databases/update_cards_database_usecase_impl.dart
+++ lib/application/usecase/source_databases/update_cards_database_usecase_impl.dart
@@ -1,19 +1,22 @@
 import 'package:marvel_snap/domain/infra/infra.dart';
 import 'package:marvel_snap/domain/usecase/usecase.dart';
 
+import '../../../infra/http/http.dart';
+
 class UpdateCardsDatabaseUsecaseImpl implements UpdateDatabaseUsecase {
   final HttpService httpService;
-  final String url;
+
   final DatabaseUpdateRepository repository;
 
   UpdateCardsDatabaseUsecaseImpl({
     required this.httpService,
-    required this.url,
     required this.repository,
   });
   @override
   Future call() async {
     try {
+      String url =
+          await ApiUrlManager.makeMarvelSnapUrl(MarvelSnapDataType.cards);
       final response = await httpService.request(
         url: url,
         method: RequestMethod.get,
--- lib/infra/factory/usecase/update_cards_database_usecase_factory.dart
+++ lib/infra/factory/usecase/update_cards_database_usecase_factory.dart
@@ -3,11 +3,8 @@ import 'package:marvel_snap/domain/usecase/usecase.dart';
 import 'package:marvel_snap/infra/factory/http/http.dart';
 import 'package:marvel_snap/infra/factory/card_database_repository_impl.dart';
 
-import '../../http/http.dart';
-
-Future<UpdateDatabaseUsecase> updateCardsDatabaseUsecaseFactory() async =>
+UpdateDatabaseUsecase updateCardsDatabaseUsecaseFactory() =>
     UpdateCardsDatabaseUsecaseImpl(
       httpService: dioAdapterFactory(),
-      url: await ApiUrlManager().makeMarvelSnapUrl(MarvelSnapDataType.cards),
       repository: cardDatabaseRepositoryFactory(),
     );
