d557b325276ae7ca2dae62d40b8d778e6137dbbd
Derek Alves <dev.derek.alves@gmail.com>
2023-02-26 15:20:28 -0300

    step: implements base server

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,20 @@
-import 'package:marvel_snap/infra/factory/usecase/usecase.dart';
+import 'package:marvel_snap/infra/env/env.dart';
+import 'package:marvel_snap/infra/factory/factory.dart';
+import 'package:marvel_snap/infra/server/server.dart';
+import 'package:shelf/shelf.dart';
 
 void main(List<String> arguments) async {
-  var updateCardsDatabase = updateCardsDatabaseUsecaseFactory();
-  var updateLocationDatabase = updateLocationDatabaseUsecaseFactory();
-  await updateLocationDatabase.call();
-  await updateCardsDatabase.call();
-  print('aaaaaaaaaaaaaaaaaaaaaaaa');
+  var cascadeHandler = Cascade()
+      .add(
+        cardRouterFactory().getHandler(),
+      )
+      .handler;
+  var handler =
+      const Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+
+  await CustomServer().initialize(
+    handler: handler,
+    address: await CustomEnv.get<String>(key: 'server_address'),
+    port: await CustomEnv.get<int>(key: 'server_port'),
+  );
 }
